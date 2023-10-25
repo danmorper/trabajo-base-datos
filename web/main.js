cd598dee87381b0ff8bce5456f4b154c61baf059
Your Name <you@example.com>
2023-10-25 20:09:12 +0200

    Hecho que aparezcan y desaparezcan las sugerencias. Otra vez soy un crack, otro piquito?

--- web/main.js
+++ web/main.js
@@ -24,31 +24,69 @@ imagenes.forEach((imagen) => {
     imagen.addEventListener("mouseout", () => {
         imagen.classList.remove("nuevaClase2");
     });
-}
-);
+});
 
 // Añadir div cuando se haga click en el botón con id "boton-sugerencia"
 
 const boton = document.querySelector("#boton-sugerencia");
 let sugerencias = document.querySelector("#anadir-sugerencias");
-boton.addEventListener("click", () => {
+let sugerencias_div = document.querySelectorAll(".sugerencia"); // Definimos la variable aquí para que esté disponible en todo el archivo
 
+let num_sugerencias = 0; // Hay 0 sugerencias
+
+boton.addEventListener("click", () => {
     // Tomo titulo y texto de textarea
     let titulo = document.querySelector("#titulo").value;
-    let texto = document.querySelector("#sugerencia").value;
+    let texto = document.querySelector("#texto-sugerencia").value;
     console.log(texto);
+    if (titulo == "" || texto == "") {
+        alert("Debes rellenar todos los campos");
+    } else {
+        num_sugerencias += 1; // Añado una sugerencia
+        console.log(num_sugerencias);
 
-    // Creo 3 div uno para el titulo, otro para el texto y otro para los dos anteriores
-    let div = document.createElement("div");
-    let div_titulo = document.createElement("div");
-    let div_texto = document.createElement("div");
-    div_titulo.innerHTML = "<h3>" + titulo + "</h3>";
-    div_texto.innerHTML = "<p>" + texto + "</p>";
-    div.appendChild(div_titulo);
-    div.appendChild(div_texto);
-    sugerencias.appendChild(div);
+        // Creo 3 div uno para el titulo, otro para el texto y otro para los dos anteriores
+        let div = document.createElement("div");
+        div.id = "divid" + num_sugerencias; // Le pongo una id para usarla luego para mostrar el texto
+        div.classList.add("sugerencia"); // Le añado la clase sugerencia para usar queryselectorall
+        div.innerHTML += "<h3 onclick='aparece(event)' id="+`titulo${num_sugerencias}>`+ titulo + "</h3>";
+        div.innerHTML += "<p id="+`texto${num_sugerencias}>` + texto + "</p>";
+        sugerencias.appendChild(div);
 
-    // Limpio titulo y textarea
-    document.getElementById("titulo").value = "";
-    document.querySelector("#sugerencia").value = "";
+        // Limpio titulo y textarea
+        document.getElementById("titulo").value = "";
+        document.querySelector("#texto-sugerencia").value = "";
+
+        // // Ocultar texto de la sugerencia que hemos añadido
+        document.getElementById(`texto${num_sugerencias}`).style.display = "none";
+
+    }
+    sugerencias_div = document.querySelectorAll(".sugerencia"); // Actualizamos la variable con las sugerencias que hemos añadido
 });
+
+//Mostrar texto cuando pinches sobre el titulo de la sugerencia
+
+// No se puede hacer asi ya que no actualiza la variable sugerencias_div. Esto se complica :(
+// sugerencias_div.forEach((sugerencia) => {
+//     console.log(sugerencia);
+//     sugerencia.addEventListener("click", () => {
+//         let titulo = sugerencia.querySelector("h3");
+//         let texto = sugerencia.querySelector("p");
+//         texto.style.display = "block";
+//     });
+// });
+
+
+// Funcion para mostrar el texto de la sugerencia
+function aparece(event) {
+    let idtitulo = event.target.id; // Cojo el id del titulo
+    console.log(idtitulo[6]);
+        let texto = document.getElementById("texto"+idtitulo[6]);
+    // console.log(texto);
+    // console.log(texto.style.display);
+    if (texto.style.display == "block") {
+        texto.style.display = "none";
+    } else {
+        texto.style.display = "block";
+    }
+}
